fc41db8a8d3d6d95ecb5b01b81bfc1524b701003
Raian Damaceno <36999338+RaianDamaceno@users.noreply.github.com>
2022-08-04 20:54:46 -0300

    Add script, e create desktop view

---
+++ script.js
@@ -0,0 +1,9 @@
+function getButtonSelectedValue(value) {
+    return value;
+}
+
+
+function setValue() {
+    document.getElementsByClassName("card-container")[0].style.display = "none";
+    document.getElementsByClassName("card-container-end")[0].style.display = "flex";
+}
